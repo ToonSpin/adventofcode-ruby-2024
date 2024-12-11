a90ca371422de6158ac242c292a9d6d3eb30e00c
Toon Spin <toon.spin@gmail.com>
2024-12-11 07:39:59 +0100

    Day 11 part 2

--- src/day11.rb
+++ src/day11.rb
@@ -1,24 +1,45 @@
 file_path = ARGV[0]
 file = File.open(file_path, "r")
 
-def process_stone(stone)
-  return [1] if stone == 0
+$stone_counts = {}
+
+def get_stone_count_single(stone)
+  return 1 if stone == 0
   num_digits = Math.log10(stone).floor + 1
-  if num_digits % 2 == 0
-    factor = 10 ** (num_digits / 2)
-    return [(stone / factor).floor, stone % factor]
-  end
-  return [stone * 2024]
+  return 2 if num_digits % 2 == 0
+  return 1
 end
 
-def iterate(input)
-  return input.map{ |stone| process_stone(stone) }.flatten
+def get_stone_count(stone, iterations)
+  key = [stone, iterations]
+
+  if !$stone_counts.include?(key)
+    if iterations == 1
+      $stone_counts[key] = get_stone_count_single(stone)
+      return $stone_counts[key]
+    end
+
+    if stone == 0
+      $stone_counts[key] = get_stone_count(1, iterations - 1)
+    else
+      num_digits = Math.log10(stone).floor + 1
+      if num_digits % 2 == 0
+        factor = 10 ** (num_digits / 2)
+        $stone_counts[key] = get_stone_count((stone / factor).floor, iterations - 1)
+        $stone_counts[key] += get_stone_count(stone % factor, iterations - 1)
+      else
+        $stone_counts[key] = get_stone_count(stone * 2024, iterations - 1)
+      end
+    end
+  end
+
+  return $stone_counts[key]
 end
 
 input = file.read.strip.split.map {|s| s.to_i}
 
-for i in 0...25
-  input = iterate(input)
-end
+count = input.map { |stone| get_stone_count(stone, 25) }
+puts count.sum
 
-puts input.length
+count = input.map { |stone| get_stone_count(stone, 75) }
+puts count.sum
