35d971ffe0f529162eefd1a3da23ea7361dee6b2
Toon Spin <toon.spin@gmail.com>
2024-12-13 18:53:10 +0100

    Day 05

--- src/day05.rb
+++ src/day05.rb
@@ -14,6 +14,20 @@ def update_matches_all?(update, rules)
   rules.all? {|rule| update_matches? update, rule}
 end
 
+def compare_update(a, b, rules)
+  rules.each do |rule|
+    first, second = rule
+    return -1 if first == a && second == b
+    return 1 if first == b && second == a
+  end
+  return 0
+end
+
+def sort_update!(update, rules)
+  result = update
+  result.sort! { |a, b| compare_update(a, b, rules) }
+end
+
 file_path = ARGV[0]
 file = File.open(file_path, "r")
 input = file.read
@@ -28,11 +42,16 @@ input.scan(/^(\d+(,\d+)*)$/).each do |update, _|
   updates.push update.split(',').map {|s| s.to_i}
 end
 
-sum = 0
+sum_correct = 0
+sum_incorrect = 0
 updates.each do |update|
+  index = update.length / 2
   if update_matches_all?(update, rules)
-    index = update.length / 2
-    sum += update[index]
+    sum_correct += update[index]
+  else
+    sort_update!(update, rules)
+    sum_incorrect += update[index]
   end
 end
-puts sum
+puts sum_correct
+puts sum_incorrect
