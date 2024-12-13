0ee8bfe7b84e2b7ea7d4454c30ff930216b1e52c
Toon Spin <toon.spin@gmail.com>
2024-12-13 09:17:22 +0100

    Day 13: improve formatting

--- src/day13.rb
+++ src/day13.rb
@@ -1,14 +1,12 @@
 class Machine
   def initialize(content)
-    button_a, button_b, prize = content.split("\n")
-
-    button_a.match /^Button .: X([+-]\d+), Y([+-]\d+)/
+    content.match /^Button A: X\+(\d+), Y\+(\d+)$/
     @a = [$1.to_i, $2.to_i]
 
-    button_b.match /^Button .: X([+-]\d+), Y([+-]\d+)/
+    content.match /^Button B: X\+(\d+), Y\+(\d+)$/
     @b = [$1.to_i, $2.to_i]
 
-    prize.match /^Prize: X=(\d+), Y=(\d+)/
+    content.match /^Prize: X=(\d+), Y=(\d+)$/
     @prize = [$1.to_i, $2.to_i]
   end
 
@@ -16,13 +14,16 @@ class Machine
     v, w = @a
     x, y = @b
     p, q = @prize
+
     if part_two
       p += 10000000000000
       q += 10000000000000
     end
+
     i = p * y - q * x
     j = q * v - p * w
     d = v * y - w * x
+
     return 0 if i % d != 0 || j % d != 0
     return (3 * i + j) / d
   end
