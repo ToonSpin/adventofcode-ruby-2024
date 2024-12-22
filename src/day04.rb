4f02b617b2dd4e09ceb022a8f52ad9eef3d85655
Toon Spin <toon.spin@gmail.com>
2024-12-22 10:58:25 +0100

    Refactor day 04

--- src/day04.rb
+++ src/day04.rb
@@ -1,48 +1,48 @@
 require_relative 'grid'
 
 class WordSearch < Grid
-  def count_xmas
-    count = 0
-    self.scan('X').each do |x, y, _|
-      (-1..1).each do |q|
-        (-1..1).each do |p|
-          next if p == 0 && q == 0
-          found = 0
-          'MAS'.split('').each_with_index do |c, i|
-            r = x + (i + 1) * p
-            s = y + (i + 1) * q
-            break if !self.in_bounds?(r, s)
-            break if self.get_cell(r, s) != c
-            found += 1
-          end
-          count += 1 if found == 3
-        end
-      end
+  def mas_at?(x, y, p, q)
+    found = 0
+    'MAS'.chars.each_with_index do |c, i|
+      r = x + ((i + 1) * p)
+      s = y + ((i + 1) * q)
+
+      break unless in_bounds?(r, s)
+      break if get_cell(r, s) != c
+
+      found += 1
     end
-    return count
+    found == 3
+  end
+
+  def count_mas_at(x, y)
+    [-1, 0, 1].product([-1, 0, 1]).count { |p, q| mas_at?(x, y, p, q) }
+  end
+
+  def mas_x_at?(x, y)
+    return false unless in_bounds?(x - 1, y - 1)
+    return false unless in_bounds?(x + 1, y + 1)
+
+    c1 = get_cell(x - 1, y - 1)
+    c2 = get_cell(x + 1, y + 1)
+    return false unless "#{c1}A#{c2}" == 'MAS' || "#{c2}A#{c1}" == 'MAS'
+
+    c1 = get_cell(x + 1, y - 1)
+    c2 = get_cell(x - 1, y + 1)
+    "#{c1}A#{c2}" == 'MAS' || "#{c2}A#{c1}" == 'MAS'
+  end
+
+  def count_xmas
+    scan('X').map { |x, y, _| count_mas_at(x, y) }.sum
   end
 
   def count_x_mas
-    count = 0
-    self.scan('A').each do |x, y, _|
-      next if !self.in_bounds?(x - 1, y - 1)
-      next if !self.in_bounds?(x + 1, y + 1)
-
-      c1 = self.get_cell(x - 1, y - 1)
-      c2 = self.get_cell(x + 1, y + 1)
-      next if "#{c1}A#{c2}" != "MAS" && "#{c2}A#{c1}" != "MAS"
-
-      c1 = self.get_cell(x + 1, y - 1)
-      c2 = self.get_cell(x - 1, y + 1)
-
-      count += 1 if "#{c1}A#{c2}" == "MAS" || "#{c2}A#{c1}" == "MAS"
-    end
-    return count
+    scan('A').count { |x, y,| mas_x_at? x, y }
   end
 end
 
 file_path = ARGV[0]
-file = File.open(file_path, "r")
+file = File.open(file_path, 'r')
 grid = WordSearch.new file.read
 
 puts grid.count_xmas
