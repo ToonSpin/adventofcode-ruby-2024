66a14d0230219c9b7709dc0a13ea0672626fa779
Toon Spin <toon.spin@gmail.com>
2024-12-22 09:52:02 +0100

    Refactor day 01

--- src/day01.rb
+++ src/day01.rb
@@ -1,31 +1,8 @@
+# frozen_string_literal: true
+
 file_path = ARGV[0]
-file = File.open(file_path, "r")
+file = File.open(file_path, 'r')
 
-left = []
-right = []
-for line in file
-    l, r = line.strip.split(/\s+/)
-    left << l.to_i
-    right << r.to_i
-end
-
-puts left.sort.zip(right.sort).map{ |a, b| (b - a).abs }.sum
-
-counts = {}
-for n in right
-    if !counts.include?(n)
-        counts[n] = 0
-    end
-    counts[n] += 1
-end
-for n in left
-    if !counts.include?(n)
-        counts[n] = 0
-    end
-end
-
-similarity_score = 0
-left.each do |n|
-    similarity_score += n * counts[n]
-end
-puts similarity_score
+left, right = file.map { |line| line.split.map(&:to_i) }.transpose
+puts left.sort.zip(right.sort).map { |a, b| (b - a).abs }.sum
+puts left.map { |n| n * right.count { |m| m == n } }.sum
