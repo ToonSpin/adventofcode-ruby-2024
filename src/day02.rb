6ce6cc854cea98124945d04f0383931ccd1be07f
Toon Spin <toon.spin@gmail.com>
2024-12-22 10:28:36 +0100

    Refactor day 02

--- src/day02.rb
+++ src/day02.rb
@@ -1,31 +1,22 @@
-def safe_in_order?(report, lower, upper)
-  for i in 0...(report.length - 1)
-    d = report[i + 1] - report[i]
-    if d < lower || d > upper
-      return false
-    end
-  end
-  return true
+# frozen_string_literal: true
+
+def safe_in_order?(report)
+  report[1..].zip(report).map { |b, a| b - a }.all? { |d| d >= 1 && d <= 3 }
 end
 
-def safe_part_one?(report, lower, upper)
-  return true if safe_in_order?(report, lower, upper)
-  return safe_in_order?(report.reverse, lower, upper)
+def safe_part_one?(report)
+  safe_in_order?(report) || safe_in_order?(report.reverse)
 end
 
-def safe_part_two?(report, lower, upper)
-  return true if safe_part_one?(report, lower, upper)
-  for i in 0...report.length
-    omitted = report[0...i] + report[i+1..report.length]
-    return true if safe_part_one?(omitted, lower, upper)
-  end
-  return false
+def safe_part_two?(report)
+  return true if safe_part_one?(report)
+
+  report.each_index.any? { |i| safe_part_one?(report[...i] + report[i + 1..]) }
 end
 
 file_path = ARGV[0]
-file = File.open(file_path, "r")
-lines = file.read.split("\n")
-reports = lines.map { |l| l.strip.split(' ').map { |n| n.to_i } }
+file = File.open(file_path, 'r')
+reports = file.map { |l| l.split.map(&:to_i) }
 
-puts reports.filter { |r| safe_part_one? r, 1, 3 }.length
-puts reports.filter { |r| safe_part_two? r, 1, 3 }.length
+puts reports.filter { |r| safe_part_one? r }.length
+puts reports.filter { |r| safe_part_two? r }.length
