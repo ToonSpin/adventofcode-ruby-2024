44801b063689b855f24bc8d2cd72f7982a169deb
Toon Spin <toon.spin@gmail.com>
2024-12-22 12:11:18 +0100

    Refactor day 07

--- src/day07.rb
+++ src/day07.rb
@@ -1,4 +1,4 @@
-require 'strscan'
+# frozen_string_literal: true
 
 class Equation
   attr_reader :test_value
@@ -6,43 +6,56 @@ class Equation
   def initialize(line)
     test_value, operands = line.strip.split(': ')
     @test_value = test_value.to_i
-    @operands = operands.split.map {|s| s.to_i}
+    @operands = operands.split.map(&:to_i)
   end
 
-  def possible?(part_two = false, aggregate = @test_value, operands = @operands.clone)
+  def possible_part_one?
+    possible?(false, @test_value, @operands.clone)
+  end
+
+  def possible_part_two?
+    possible?(true, @test_value, @operands.clone)
+  end
+
+  def add_possible?(part_two, aggregate, operands)
+    return false unless aggregate >= operands.last
+
+    operand = operands.pop
+    possible?(part_two, aggregate - operand, operands.clone)
+  end
+
+  def mul_possible?(part_two, aggregate, operands)
+    return false unless (aggregate % operands.last).zero?
+
+    operand = operands.pop
+    possible?(part_two, aggregate / operand, operands.clone)
+  end
+
+  def concat_possible?(part_two, aggregate, operands)
+    return false unless part_two
+
     operand = operands.pop
 
-    if operands.length == 0
-      return operand == aggregate
-    end
+    digits = Math.log10(operand).to_i + 1
+    p10 = 10**digits
+    return false unless aggregate % p10 == operand
 
-    if part_two
-      digits = Math.log10(operand).to_i + 1
-      p10 = 10 ** digits
-      if aggregate % p10 == operand
-        return true if self.possible?(part_two, aggregate / p10, operands.clone)
-      end
-    end
+    possible?(part_two, aggregate / p10, operands.clone)
+  end
 
-    if aggregate % operand == 0
-      return true if self.possible?(part_two, aggregate / operand, operands.clone)
-    end
+  def possible?(part_two, aggregate, operands)
+    return operands[0] == aggregate if operands.length == 1
+    return true if concat_possible?(part_two, aggregate, operands.clone)
+    return true if mul_possible?(part_two, aggregate, operands.clone)
 
-    if aggregate >= operand
-      return true if self.possible?(part_two, aggregate - operand, operands.clone)
-    end
-
-    return false
+    add_possible?(part_two, aggregate, operands.clone)
   end
 end
 
 file_path = ARGV[0]
-file = File.open(file_path, "r")
+file = File.open(file_path, 'r')
 
-equations = []
-file.each do |line|
-  equations << Equation.new(line)
-end
+equations = file.map { |line| Equation.new(line) }
 
-puts equations.filter {|e| e.possible?}.map {|e| e.test_value}.sum
-puts equations.filter {|e| e.possible?(true)}.map {|e| e.test_value}.sum
+puts equations.filter(&:possible_part_one?).map(&:test_value).sum
+puts equations.filter(&:possible_part_two?).map(&:test_value).sum
