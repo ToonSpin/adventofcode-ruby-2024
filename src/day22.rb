f4d4287d5528c7c460c9fe7d628a162b175ee119
Toon Spin <toon.spin@gmail.com>
2024-12-22 09:27:38 +0100

    Day 22 part 2

--- src/day22.rb
+++ src/day22.rb
@@ -19,7 +19,37 @@ def evolve(secret)
   secret
 end
 
+def get_index(changes)
+  changes.inject { |a, e| (e + 9) + (19 * a) }
+end
+
+def get_map(secret)
+  evolutions = [secret % 10]
+  2000.times do
+    secret = evolve_once(secret)
+    evolutions << (secret % 10)
+  end
+
+  diffs = (1...evolutions.length).map { |i| evolutions[i] - evolutions[i - 1] }
+
+  prices_map = {}
+  (0..diffs.length - 4).each do |i|
+    changes = get_index(diffs[i..i + 3])
+    prices_map[changes] = evolutions[i + 4] unless prices_map.include? changes
+  end
+
+  prices_map
+end
+
+def get_winnings(maps, key)
+  maps.map { |m| m[key] }.compact.sum
+end
+
 file_path = ARGV[0]
 file = File.open(file_path, 'r')
 
-puts file.map(&:to_i).map(&method(:evolve)).sum
+secrets = file.map(&:to_i)
+puts secrets.map(&method(:evolve)).sum
+
+maps = secrets.map(&method(:get_map))
+puts maps.map(&:keys).flatten.to_set.map { |k| get_winnings(maps, k) }.max
